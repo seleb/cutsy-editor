7f275db00c58d5eb5d4a118af278f29679d9b8ce
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-08 21:45:44 -0400

    remove sample command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,10 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[tauri::command]
 fn is_ffmpeg_installed() -> bool {
@@ -21,7 +17,6 @@ fn install_ffmpeg() {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            greet,
             is_ffmpeg_installed,
             install_ffmpeg,
         ])
