3e55bb2bd0b6f8a39f527e1330d15b64903f5a98
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-09 19:43:13 -0400

    automatically open devtools in debug builds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 #[tauri::command]
@@ -16,6 +18,16 @@ fn install_ffmpeg() {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            // automatically open devtools in debug builds
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             is_ffmpeg_installed,
             install_ffmpeg,
