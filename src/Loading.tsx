9f3ffcc1503a8df7d481b67f398a74f18f32114c
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-10 20:39:56 -0400

    add count + "none" state to loading element

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -4,20 +4,27 @@ import { Spinner } from "./Spinner";
 export function Loading({
 	loading = false,
 	error = false,
+	count,
 	msgLoading = '',
 	msgError = 'Something went wrong',
+	msgNone = 'Nothing found',
 	Error = ({ children }) => <>{children}</>,
 	Loading = Spinner,
+	None = ({ children }) => <>{children}</>,
 	children,
 }: PropsWithChildren<{
 	loading?: boolean;
 	error?: boolean;
+	count?: number;
 	msgLoading?: ReactNode;
 	msgError?: ReactNode;
+	msgNone?: ReactNode;
 	Error?: ElementType;
 	Loading?: ElementType;
+	None?: ElementType;
 }>) {
 	if (error) return <Error>{msgError}</Error>
 	if (loading) return <Loading>{msgLoading}</Loading>
+	if (count === 0) return <None>{msgNone}</None>
 	return <>{children}</>;
 }
