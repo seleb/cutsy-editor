d7baca2b1ef01833d3d1fde3a07a9bc54287bd11
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-13 19:10:06 -0400

    Create ContextSettings.tsx

---
+++ src/ContextSettings.tsx
@@ -0,0 +1,79 @@
+import { ask } from '@tauri-apps/api/dialog';
+import { Dispatch, PropsWithChildren, createContext, useCallback, useContext, useEffect, useReducer } from 'react';
+
+type State = {
+	theme: 'auto' | 'dark' | 'light';
+	font: 'Bitch' | 'Boring';
+	openAfterSave: 'true' | 'false';
+	videoFolders: string[];
+	saveAudio: 'always' | 'never' | 'editor';
+};
+
+const initial: State = {
+	theme: 'auto',
+	font: 'Bitch',
+	openAfterSave: 'true',
+	videoFolders: [],
+	saveAudio: 'always',
+};
+
+type Action<T extends keyof State> =
+	| {
+			type: 'set';
+			key: T;
+			value: State[T];
+	  }
+	| {
+			type: 'reset';
+	  };
+
+function reducer(state: State, action: Action<keyof State>) {
+	switch (action.type) {
+		case 'reset':
+			return { ...initial };
+		case 'set':
+			return {
+				...state,
+				[action.key]: action.value,
+			};
+		default:
+			throw new Error('Unsupported action');
+	}
+}
+
+const contextState = createContext<State>(initial);
+const contextDispatch = createContext<Dispatch<Action<keyof State>>>(() => {});
+
+export function ContextSettings({ children }: PropsWithChildren<{}>) {
+	const [state, dispatch] = useReducer(reducer, initial);
+
+	useEffect(() => {
+		document.documentElement.dataset.font = state.font;
+	}, [state.font]);
+	useEffect(() => {
+		document.documentElement.dataset.theme = state.theme;
+	}, [state.theme]);
+
+	return (
+		<contextState.Provider value={state}>
+			<contextDispatch.Provider value={dispatch}>{children}</contextDispatch.Provider>
+		</contextState.Provider>
+	);
+}
+
+export function useSettings() {
+	return useContext(contextState);
+}
+
+export function useSettingsSet() {
+	const dispatch = useContext(contextDispatch);
+	return useCallback(<T extends keyof State>(key: T, value: State[T]) => dispatch({ type: 'set', key, value }), [dispatch]);
+}
+
+export function useSettingsReset() {
+	const dispatch = useContext(contextDispatch);
+	return useCallback(async () => {
+		if (!await ask('Are you sure? This action cannot be undone.')) return;
+		dispatch({ type: 'reset' });
+	}, [dispatch]);
+}
