7a548eb1eb76d069f44b6609359283010f76693a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-13 16:23:17 -0400

    put an index back in with a redirect to videos

---
+++ src/ViewIndex.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+export function ViewIndex() {
+	const navigate = useNavigate();
+	useEffect(() => {
+		navigate('videos', { replace: true });
+	}, []);
+	return null;
+}
