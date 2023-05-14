9bf7a0ea01d21639a91f442b27e8ee7566250ccd
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-14 00:55:10 -0400

    rough out "app" context

---
+++ src/ContextApp.tsx
@@ -0,0 +1,99 @@
+import { Dispatch, PropsWithChildren, createContext, useCallback, useContext, useReducer } from 'react';
+
+type Command = {
+	command: 'vid_to_img'
+	input: string;
+	output: string;
+	time: number;
+} | {
+	command: 'vid_to_clip'
+	input: string;
+	output: string;
+	start: number;
+	duration: number;
+	audio: boolean;
+};
+
+type State = {
+	/** last known video state */
+	video: {
+		path: string;
+		clipStart: number;
+		clipEnd: number;
+	};
+	queue: Command[];
+};
+
+const initial: State = {
+	video: {
+		path: '',
+		clipStart: 0,
+		clipEnd: 1,
+	},
+	queue: [],
+};
+
+type Action =
+	| {
+			type: 'video';
+			video?: State['video'];
+	  }
+	| {
+			type: 'queue-push';
+			command: Command;
+	}
+	| {
+			type: 'queue-shift';
+	};
+
+function reducer(state: State, action: Action) {
+	switch (action.type) {
+		case 'video':
+			return { ...state, video: { ...initial.video, ...action.video } };
+		case 'queue-push':
+			return {
+				...state,
+				queue: state.queue.concat(action.command),
+			};
+		case 'queue-shift':
+			return {
+				...state,
+				queue: state.queue.slice(1),
+			};
+		default:
+			throw new Error('Unsupported action');
+	}
+}
+
+const contextState = createContext<State>(initial);
+const contextDispatch = createContext<Dispatch<Action>>(() => {});
+
+export function ContextApp({ children }: PropsWithChildren<{}>) {
+	const [state, dispatch] = useReducer(reducer, initial);
+
+	// TODO: queue management
+
+	return (
+		<contextState.Provider value={state}>
+			<contextDispatch.Provider value={dispatch}>{children}</contextDispatch.Provider>
+		</contextState.Provider>
+	);
+}
+
+export function useQueue() {
+	return useContext(contextState).queue;
+}
+
+export function useVideo() {
+	return useContext(contextState).video;
+}
+
+export function useVideoSet() {
+	const dispatch = useContext(contextDispatch);
+	return useCallback((video?: State['video']) => dispatch({ type: 'video', video }), [dispatch]);
+}
+
+export function useQueuePush() {
+	const dispatch = useContext(contextDispatch);
+	return useCallback((command: Command) => dispatch({ type: 'queue-push', command }), [dispatch]);
+}
