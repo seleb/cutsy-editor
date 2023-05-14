a528fe52d08481ca995a6569c90c0f6bcaf4715d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-14 13:26:39 -0400

    unnecessary else

--- src/getErrorMessage.ts
+++ src/getErrorMessage.ts
@@ -1,5 +1,5 @@
 export function getErrorMessage(err: unknown) {
 	if (err instanceof Error) return err.message;
-	else if (typeof err === 'string') return err;
-	else return 'Unknown error';
+	if (typeof err === 'string') return err;
+	return 'Unknown error';
 }
