6bd8812e96b4ef7baa512f0d8a5c9e079b1b029b
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-14 18:00:00 -0400

    refactor videos to use app state + search

--- src/ViewVideos.tsx
+++ src/ViewVideos.tsx
@@ -1,5 +1,6 @@
 import { FileEntry, readDir } from '@tauri-apps/api/fs';
 import { BaseDirectory } from '@tauri-apps/api/path';
+import { open } from '@tauri-apps/api/shell';
 import { convertFileSrc } from '@tauri-apps/api/tauri';
 import {
 	ChangeEventHandler,
@@ -8,26 +9,26 @@ import {
 	useEffect,
 	useLayoutEffect,
 	useMemo,
-	useRef,
 	useState,
 } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { usePagination } from 'react-use-pagination';
-import { open } from '@tauri-apps/api/shell';
 import {
-	AllSubstringsIndexStrategy,
-	Search,
-	UnorderedSearchIndex,
-} from 'js-search';
+	Link,
+	useNavigate,
+	useParams,
+	useSearchParams,
+} from 'react-router-dom';
+import { usePagination } from 'react-use-pagination';
+import { useSearch, useVideos, useVideosSet } from './ContextApp';
+import { useSettings } from './ContextSettings';
 import { H } from './H';
+import { Icon } from './Icon';
 import { Loading } from './Loading';
 import { Page } from './Page';
 import { PageHeader } from './PageHeader';
 import { PageNumbers } from './PageNumbers';
 import { Title } from './Title';
 import styles from './ViewVideos.module.scss';
-import { useSettings } from './ContextSettings';
-import { Icon } from './Icon';
+import { clamp } from './clamp';
 import { getErrorMessage } from './getErrorMessage';
 import { isVideo } from './isVideo';
 import { toEditUrl } from './toEditUrl';
@@ -61,61 +62,68 @@ function Video({ path, name }: FileEntry) {
 	);
 }
 export function ViewVideos() {
+	const [searchParams] = useSearchParams();
 	const { videoFolders, itemsPerPage } = useSettings();
 	const { page } = useParams();
-	const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState('');
-	const [library, setLibrary] = useState<FileEntry[]>([]);
-	const [results, setResults] = useState<FileEntry[]>([]);
-	const refSearch = useRef<Search>(null);
-	useEffect(() => {
-		(async () => {
-			try {
-				setLoading(true);
-				const search = new Search('path');
-				search.searchIndex = new UnorderedSearchIndex();
-				search.indexStrategy = new AllSubstringsIndexStrategy();
-				search.addIndex('name');
-				search.addIndex('path');
-				let dirs: FileEntry[][];
-				if (videoFolders.length) {
-					dirs = await Promise.all(
-						videoFolders.map((i) => readDir(i, { recursive: true }))
-					);
-				} else {
-					dirs = [
-						await readDir('.', { dir: BaseDirectory.Video, recursive: true }),
-					];
-				}
-				const flatten = (dir: FileEntry): FileEntry[] =>
-					dir.children ? dir.children.flatMap(flatten) : [dir];
-				const files = dirs
-					.flatMap((i) => i.flatMap(flatten))
-					.filter((i) => isVideo(i.path));
-				setLibrary(files);
-				setResults(files);
-				search.addDocuments(files);
-				// TS makes refs readonly, but this is written imperatively on purpose
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
-				refSearch.current = search;
-				setLoading(false);
-			} catch (err) {
-				console.error(err);
-				setError(getErrorMessage(err));
-			}
-		})();
-	}, [videoFolders]);
+	const videos = useVideos();
+	const setVideos = useVideosSet();
+	const search = useSearch();
+	const [query, setQuery] = useState(searchParams.get('q') || '');
+	const [sort, setSort] = useState<'sortAsc' | 'sortDesc' | 'sortNone'>(
+		'sortNone'
+	);
+	const navigate = useNavigate();
 
 	const numPage = useMemo(() => {
 		const num = parseInt(page || '0', 10);
 		if (Number.isNaN(num)) return 0;
 		return num;
 	}, [page]);
+	const lastPage = usePrevious(numPage) || -1;
 
-	const [sort, setSort] = useState<'sortAsc' | 'sortDesc' | 'sortNone'>(
-		'sortNone'
+	const filteredVideos = useMemo(
+		() => (query ? search(query) : videos),
+		[query, search, videos]
 	);
+	const sorted = useMemo(() => {
+		if (sort === 'sortNone') return filteredVideos;
+		const copy = filteredVideos.slice();
+		copy.sort(({ name: a = '' }, { name: b = '' }) =>
+			a?.localeCompare(b, undefined, { sensitivity: 'base' })
+		);
+		if (sort === 'sortDesc') copy.reverse();
+		return copy;
+	}, [filteredVideos, sort]);
+
+	const loadVideos = useCallback(async () => {
+		try {
+			setLoading(true);
+			let dirs: FileEntry[][];
+			if (videoFolders.length) {
+				dirs = await Promise.all(
+					videoFolders.map((i) => readDir(i, { recursive: true }))
+				);
+			} else {
+				dirs = [
+					await readDir('.', { dir: BaseDirectory.Video, recursive: true }),
+				];
+			}
+			const flatten = (dir: FileEntry): FileEntry[] =>
+				dir.children ? dir.children.flatMap(flatten) : [dir];
+			const files = dirs
+				.flatMap((i) => i.flatMap(flatten))
+				.filter((i) => isVideo(i.path));
+			setVideos(files);
+		} catch (err) {
+			console.error(err);
+			setError(getErrorMessage(err));
+		} finally {
+			setLoading(false);
+		}
+	}, [setVideos, videoFolders]);
+
 	const cycleSort = useCallback(() => {
 		setSort(
 			(s) =>
@@ -128,54 +136,40 @@ export function ViewVideos() {
 				)[s])
 		);
 	}, []);
-	const sorted = useMemo(() => {
-		if (sort === 'sortNone') return results;
-		const copy = results.slice();
-		copy.sort(({ name: a = '' }, { name: b = '' }) =>
-			a?.localeCompare(b, undefined, { sensitivity: 'base' })
-		);
-		if (sort === 'sortDesc') copy.reverse();
-		return copy;
-	}, [results, sort]);
 
 	const { totalPages, startIndex, endIndex, setPage } = usePagination({
 		totalItems: sorted.length,
 		initialPage: numPage,
 		initialPageSize: itemsPerPage || 1,
 	});
-	useLayoutEffect(() => {
-		setPage(numPage);
-	}, [numPage, setPage, totalPages]);
 
-	const navigate = useNavigate();
 	const goto = useCallback(
 		(newPage: number) => {
 			if (Number.isNaN(newPage)) return;
-			navigate(`/videos/${newPage}`);
+			navigate(`/videos/${clamp(0, newPage, totalPages - 1)}?q=${query}`);
 		},
-		[navigate]
+		[navigate, query, totalPages]
 	);
 
 	const onSearch = useCallback<ChangeEventHandler<HTMLInputElement>>(
 		(event) => {
-			const search = refSearch.current;
-			if (!search) return;
-			const query = event.currentTarget.value;
-			if (!query) {
-				setResults(library);
-				navigate('/videos/0', { replace: true });
-				return;
-			}
-			const files = search.search(event.currentTarget.value) as FileEntry[];
-			setResults(files);
-			navigate('/videos/0', { replace: true });
+			setQuery(event.currentTarget.value);
+			navigate(`/videos/0?q=${event.currentTarget.value}`, { replace: true });
 		},
-		[library, navigate]
+		[navigate]
 	);
+
+	// load videos on first view if there are none
+	useEffect(() => {
+		if (videos.length) return;
+		loadVideos();
+	}, [videos, loadVideos]);
+	useLayoutEffect(() => {
+		setPage(numPage);
+	}, [numPage, setPage]);
 
 	// scroll down if going back 1 page,
 	// otherwise scroll up if changing page
-	const lastPage = usePrevious(numPage) || -1;
 	useEffect(() => {
 		if (Math.abs(numPage - lastPage) === 1 && numPage < lastPage) {
 			document.documentElement.scrollTo({
@@ -193,27 +187,31 @@ export function ViewVideos() {
 				<H className={styles.h}>videos</H>
 
 				<datalist id="list-videos">
-					{library.map((i) => (
+					{videos.map((i) => (
 						// datalist doesn't need label
 						// eslint-disable-next-line jsx-a11y/control-has-associated-label
 						<option key={i.path} value={i.name} />
 					))}
 				</datalist>
+				<button type="button" onClick={loadVideos} title="Refresh videos">
+					<Icon icon="refresh" />
+				</button>
 				<div className={styles.search}>
 					<Icon icon="search" />
 					<input
 						type="text"
 						list="list-videos"
 						placeholder="Search..."
+						value={query}
 						onChange={onSearch}
 					/>
 				</div>
 
 				<span className={styles.count}>
-					{results.length
+					{sorted.length
 						.toString(10)
-						.padStart(library.length.toString(10).length, '0')}{' '}
-					/&nbsp;{library.length}
+						.padStart(videos.length.toString(10).length, '0')}{' '}
+					/&nbsp;{videos.length}
 				</span>
 				<button
 					type="button"
@@ -247,7 +245,7 @@ export function ViewVideos() {
 						Failed to load videos :(<pre>{error}</pre>
 					</>
 				}
-				count={results.length}
+				count={sorted.length}
 				msgNone="No videos ¯\_(ツ)_/¯"
 			>
 				<ul className={styles.videos}>
